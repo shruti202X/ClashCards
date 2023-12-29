92d19e83a2feb16b6a72a849899b3a31dcec9be7
shruti202X <87233649+shruti202X@users.noreply.github.com>
2023-12-29 04:27:24 +0000

    Added style to modal

--- src/components/ModalNewCard.jsx
+++ src/components/ModalNewCard.jsx
@@ -1,26 +1,27 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import "./ModalNewCard.css";
 
 export default function ModalNewCard(props) {
   return (
-    <Modal {...props} centered>
+    <Modal {...props} centered contentClassName="neon-modal">
       <Modal.Header closeButton>
         <Modal.Title>Create a New Subject</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Group className="mb-3">
             <Form.Label>Enter subject</Form.Label>
             <Form.Control type="text" placeholder="Mathematics" autoFocus />
           </Form.Group>
         </Form>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={props.onHide}>
+      <Modal.Footer className="footer">
+        <Button className="btn close" onClick={props.onHide}>
           Close
         </Button>
-        <Button variant="primary" onClick={props.onHide}>
+        <Button className="btn save" onClick={props.onHide}>
           Save Changes
         </Button>
       </Modal.Footer>
